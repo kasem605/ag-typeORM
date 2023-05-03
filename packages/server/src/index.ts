1a0fc648d1980a0438ae6757d12ede6787cff875
kasem605 <kasem_i@yahoo.com>
2023-05-03 09:29:10 -0700

    Add graphql folders and related file

---
+++ packages/server/src/index.ts
@@ -0,0 +1,35 @@
+import express, {Application} from "express";
+import {ApolloServer, Config, gql} from "apollo-server-express";
+import { IResolvers } from "@graphql-tools/utils";
+
+const typeDefs = gql `
+    type Query {
+        message: String!
+    }
+`
+
+const resolvers: IResolvers = {
+    Query: {
+        message: () => "It works!"
+    }
+}
+
+const config: Config = {
+    typeDefs: typeDefs,
+    resolvers: resolvers
+};
+
+async function startApolloServer(config: Config) {
+    const PORT = 8080;
+    const app: Application = express();
+    const server: ApolloServer =  new ApolloServer(config);
+    await server.start();
+    server.applyMiddleware({
+        app,
+        path: "/graphql"
+    });
+    app.listen(PORT, () => {
+        console.log(`server is running a http://localhost:${PORT}`);
+    });
+}
+startApolloServer(config);
